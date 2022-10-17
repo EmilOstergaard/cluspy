76a3d7214b721af12864ef7e1f0d1c803708a1f2
Emil Østergaard <ee.ostergaard@gmail.com>
2022-10-17 15:55:39 +0200

    setup.py file updated

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
 
 setup(
-    name='cluscompy',
-    packages=find_packages(include=['cluspy']),
-    version='0.1.0',
-    description='Library for simulating cluster generation and computation for Measurement-Based Quantum Computation (MBQC).',
-    author='Emil Ostergaard',
-    license='MIT',
+  name = 'cluscompy',         
+  packages = ['cluscompy'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Library for simulating cluster generation and computation for Measurement-Based Quantum Computation (MBQC).',   
+  author = 'Emil Ostergaard',                   
+  author_email = 'ee.ostergaard@gmail.com',     
+  url = 'https://github.com/EmilOstergaard/cluspy',   
+  download_url = '',    
+  keywords = ['cluster', 'quantum', 'measurement-based quantum computation'],   
+  install_requires=[            
+          '',
+          '',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+  ],
 )
